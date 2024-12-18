454069db09aa2a4d23225b2a471de386ab09e9c4
Jacinto Gutierrez <jjgc.2499@gmail.com>
2024-12-17 22:01:39 -0500

    Exercice 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,6 @@
+/**
+ * Exercices Part 1 (1.1.-1.2)
+
 const Header = ({ course }) => (
   <div>
     <h1>{course}</h1>
@@ -44,6 +47,59 @@ const App = () => {
         exercises={[exercises1, exercises2, exercises3]}
       />
       <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+export default App;
+*/
+
+import { useState } from "react";
+
+const Text = (props) => (
+  <div>
+    <h1>{props.value}</h1>
+  </div>
+);
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Data = ({ value, feedback }) => {
+  return (
+    <p>
+      {value}: {feedback}
+    </p>
+  );
+};
+
+const Content = ({ value, feedback }) => {
+  return (
+    <div>
+      {value.map((item, index) => (
+        <Data key={index} value={item} feedback={feedback[index]} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Text value="Give Feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Text value="Statistic" />
+      <Content
+        value={["Good", "Neutral", "Bad"]}
+        feedback={[good, neutral, bad]}
+      />
     </div>
   );
 };
