3c0c283efca4b2539f26782fe10064dbbb94b428
Jacinto Gutierrez <jjgc.2499@gmail.com>
2025-06-12 17:07:42 -0500

    ejercicio 2.4 courseinfo

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,18 +1,16 @@
 const Course = (props) => {
-  const { courses } = props;
-  
+	const { courses } = props;
+
 	return (
 		<div>
 			{courses.map((course) => (
 				<div key={course.id}>
 					<h2>{course.name}</h2>
-					<ul>
-						{course.parts.map((part) => (
-							<li key={part.id}>
-								{part.name} {part.exercises}
-							</li>
-						))}
-					</ul>
+					{course.parts.map((part) => (
+						<p key={part.id}>
+							{part.name} {part.exercises}
+						</p>
+					))}
 					<strong>
 						Total of{" "}
 						{course.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
