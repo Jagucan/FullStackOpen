32ac66f9b1c49b9c1487ef28bfb9d42f62457e23
Jacinto Gutierrez <jjgc.2499@gmail.com>
2025-06-12 11:15:39 -0500

    ejercicio 1.7: unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-const History = (props) => {
-	if (props.allClicks.length === 0) {
-		return <div>the app is used by pressing the buttons</div>;
-	}
-	return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
 const Button = (props) => {
-	console.log("props value is", props);
 	const { handleClick, text } = props;
 	return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+	const totalScore = good * 1 + neutral * 0 + bad * -1;
+	const average = total !== 0 ? totalScore / total : 0;
+	const positive = total !== 0 ? (good / total) * 100 : 0;
+
+	return (
+		<div>
+			<p>
+				good {good} <br />
+				neutral {neutral} <br />
+				bad {bad} <br />
+				all {total} <br />
+				average {average} <br />
+				positive {positive} %
+			</p>
+		</div>
+	);
 };
 
 const App = () => {
@@ -18,22 +30,18 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
-	const [allClicks, setAll] = useState([]);
 
 	const handleGoodClick = () => {
-		setAll(allClicks.concat("G"));
 		const updatedGood = good + 1;
 		setGood(updatedGood);
 	};
 
 	const handleNeutralClick = () => {
-		setAll(allClicks.concat("N"));
 		const updatedNeutral = neutral + 1;
 		setNeutral(updatedNeutral);
 	};
 
 	const handleBadClick = () => {
-		setAll(allClicks.concat("B"));
 		const updatedBad = bad + 1;
 		setBad(updatedBad);
 	};
@@ -45,11 +53,7 @@ const App = () => {
 			<Button handleClick={handleNeutralClick} text="neutral" />
 			<Button handleClick={handleBadClick} text="bad" />
 			<h2>statistics</h2>
-			<p>good {good} </p>
-			<p>neutral {neutral} </p>
-			<p>bad {bad} </p>
-			<p>all clicks: {allClicks.join(" ")}</p>
-			<History allClicks={allClicks} />
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	);
 };
